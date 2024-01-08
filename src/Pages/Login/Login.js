6bccc4604afbaecf72ca340a3532c6ac5a09d4a4
Jishan7866 <ansarij7866@gmail.com>
2024-01-08 23:04:48 +0530

    add design

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import '../../App.css'
+import logo from '../../Img/Logo.png'
 export default function Login(){
     return(
-        <div>
-            <h3>Login Page</h3>
+        <div className='Page'>
+            <div className='Leftside'>
+                <img src={logo} className='Img'></img>
+            </div>
+            <div className='Rightside'>
+                  <h1>Welcome To Tasker</h1>
+                  <h2>Please, insert your informations to access your tasks</h2>
+                  <label className='FieldName'>Email</label>
+                  <input className='input-field' id='email' placeholder='your email'></input>
+                  <label className='FieldName'>Password</label>
+                  <input className='input-field' type='password' placeholder='your password'></input>
+            </div>
         </div>
     )
 }
