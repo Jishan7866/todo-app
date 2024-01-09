eec3ba43f25c4fcc2254199852266ec2979f7dc4
Jishan7866 <ansarij7866@gmail.com>
2024-01-09 22:44:10 +0530

    add sign up page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Login from './Pages/Login/Login';
+import SignUp from './Pages/SignUp/SignUp';
 
 function App() {
   return (
-   <Login></Login>
+  //  <Login></Login>
+  <SignUp></SignUp>
   );
 }
 
---
+++ src/Pages/SignUp/SignUp.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import '../../App.css'
+import logo from '../../Img/Logo.png'
+export default function SignUp(){
+    return(
+        <div className='Page'>
+            <div className='Leftside'>
+                <img src={logo} className='Img'></img>
+            </div>
+            <div className='Rightside'>
+                  <h1>Welcome To Tasker</h1>
+                  <h2 className='register'>Sign Up</h2>
+                  <label className='FieldName'>Name</label>
+                  <input className='input-field' id='email' placeholder='your Name'></input>
+                  <label className='FieldName'>Email</label>
+                  <input className='input-field' id='email' placeholder='your email'></input>
+                  <label className='FieldName'>Password</label>
+                  <input className='input-field' type='password' placeholder='your password'></input>
+                  <label className='FieldName'>Confirm Password</label>
+                  <input className='input-field' type='password' placeholder='Retype password'></input>
+                  <div className='KeepSigned'>
+                        <input type='checkbox' className='checkbox'></input><h2>I agree to the terms and conditions.</h2>
+                  </div>
+                  <button type='submit' className='SignIn'>Sign Up</button>
+                  <h2>Do you already have an account? <a>Sign In</a></h2>
+            </div>
+        </div>
+    )
+}
