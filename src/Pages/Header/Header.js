a8833c368c062f2a6524172321ade9baa25a3243
Jishan7866 <ansarij7866@gmail.com>
2024-01-11 23:33:00 +0530

    add login drop down

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -1,10 +1,23 @@
-import react from 'react';
+import React, {useState} from 'react';
 import avatar from '../../Img/avatar.jpg';
 export default function Header(){
+    const [isDropdownOpen,setDropdownOpen]=useState(false);
+    const toggleDropdown = () =>{
+        setDropdownOpen(!isDropdownOpen);
+    }
     return(
         <header className='header-container'>
-            <div className='avatar-container'>
-                <img className='avatar-image' src={avatar}></img>
+            <div className='avatar-container' onClick={toggleDropdown}>
+                <img className='avatar-image' src={avatar} alt='avatar'></img>
+                {isDropdownOpen &&(
+                    <div className=''>
+                        <ul>
+                            <li>My Profile</li>
+                            <li>My Prof</li>
+                            <li>My Profile</li>
+                        </ul>
+                    </div>
+                )}
             </div>
             <h1 className='header-title'>Welcome to Admin In Todo-App</h1>
         </header>
