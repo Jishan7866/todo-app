905ea60e521b605984c7780b8dad77e32aeeae5e
Jishan7866 <ansarij7866@gmail.com>
2024-01-17 21:55:00 +0530

    add mantine and tabler ico install

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,30 @@
 import React from 'react'
-import Header from '../Header/Header'
+import {
+	Button,
+	Container,
+	Text,
+	Title,
+	Modal,
+	TextInput,
+	Group,
+	Card,
+	ActionIcon,
+	Code,
+} from '@mantine/core';
+import { useState, useRef, useEffect } from 'react';
+import { MoonStars, Sun, Trash } from 'tabler-icons-react';
+
+import {
+	MantineProvider,
+	ColorSchemeProvider,
+	ColorScheme,
+} from '@mantine/core';
+import { useColorScheme } from '@mantine/hooks';
+import { useHotkeys, useLocalStorage } from '@mantine/hooks';
 export default function Home(){
     return(
         <div>
-            <Header></Header>
+            
         </div>
     )
 }
